1ed89d8b2dbcc7411064f73cae2f21092c7b4f7e
Vadim Vasilkov <9307784586@mail.ru>
2024-05-05 10:38:59 +0300

    5

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { SequelizeConfigService } from './config/sequelizeConfig.service';
+import { databaseConfig } from './config/configuration';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequelizeConfigService,
+      
+    }),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    
+     UsersModule],
+    
 
 })
 export class AppModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users.model';
 
 @Module({
+  imports: [ SequelizeModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService]
 })
